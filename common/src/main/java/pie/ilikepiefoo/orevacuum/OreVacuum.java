674828495c3b8137809ee20f3ecb4485e5f09779
Hunter19823 <hspragg@asu.edu>
2024-01-09 14:05:00 -0700

    Did a little bit of cleanup

--- common/src/main/java/pie/ilikepiefoo/orevacuum/OreVacuum.java
+++ common/src/main/java/pie/ilikepiefoo/orevacuum/OreVacuum.java
@@ -1,10 +1,17 @@
 package pie.ilikepiefoo.orevacuum;
 
+import net.minecraft.resources.ResourceLocation;
+import pie.ilikepiefoo.orevacuum.registry.Items;
+
 public class OreVacuum {
     public static final String MOD_ID = "orevacuum";
     public static final String MOD_NAME = "Ore Vacuum";
 
     public static void init() {
-        Registry.init();
+        Items.init();
+    }
+
+    public static ResourceLocation id(String path) {
+        return new ResourceLocation(MOD_ID, path);
     }
 }
